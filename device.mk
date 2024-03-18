2f1c74bcc7ff5256f2f390fda1d70ef5e4e84433
Jooyung Han <jooyung@google.com>
2024-03-18 14:23:49 +0900

    X6886: Install apex for clearkey service
    Bug: 329737888
    Test: local build
    Change-Id: Ibf5574ce7da0b927ef8179663bb03a039955bcd3
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -156,7 +156,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    com.android.hardware.drm.clearkey
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
