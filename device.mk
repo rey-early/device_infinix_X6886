e6fc6d2a79a41710616756c800462bb9c5dd8835
claxten10 <claxten10@gmail.com>
2025-12-04 00:32:15 +0900

    X6886: Switch to source-built C2 service
    Change-Id: Ic3aa378363c2a7af1e7ea049467f10da196964f3
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -271,13 +271,9 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker.config.json
     
 # Media
+$(call soong_config_set_bool,android_hardware_mediatek_codec2,link_v33_libstagefright_foundation,true)
 PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0.vendor:64 \
-    android.hardware.media.c2@1.1.vendor:64 \
-    android.hardware.media.c2@1.2.vendor:64 \
-    android.hardware.cas@1.2-service-lazy \
-    libcodec2_hidl@1.2.vendor:64 \
-    libcodec2_hidl_plugin:64 \
+    android.hardware.media.c2-mtk-service \
     libcodec2_vndk.vendor:64 \
     libeffects:64 \
     libeffectsconfig.vendor:64 \
@@ -285,15 +281,14 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx_plugin.vendor:64 \
     libsfplugin_ccodec_utils.vendor:64 \
     libcodec2_soft_common.vendor:64 \
-    libflatbuffers-cpp.vendor:64
-
-PRODUCT_PACKAGES += \
-    libchrome.vendor:64 \
+    libflatbuffers-cpp.vendor:64 \
     libminijail:64 \
     libminijail.vendor:64
 
+PRODUCT_PACKAGES += \
+    android.hardware.cas@1.2-service-lazy
+
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Neural networks
